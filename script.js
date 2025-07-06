420a22f0a31613bf6399d069b90498cc295c86b4
exsetupplg1 <a.vitelli007@gmail.com>
2025-07-06 22:48:30 +0700

    Update script.js
    perubahan ke 3

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-   window.onload = () => {
-      const loading = document.getElementById("loading");
-      const konten = document.getElementById("konten");
+     const box = document.getElementById("peringatan");
+  box.style.display = "block";
 
-      loading.style.opacity = 0;
-
-      // Setelah transisi selesai, sembunyikan loading dan tampilkan konten
-      setTimeout(() => {
-        loading.style.display = "none";
-        konten.style.visibility = "visible";
-      }, 500); // waktu transisi CSS
-    };
+  setTimeout(() => {
+    box.style.display = "none";
+  }, 5000);
